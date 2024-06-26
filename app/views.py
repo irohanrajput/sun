ac9dd4af79196e139857c45e57a12fff1ca812df
irohanrajput <itusharsingh27@gmail.com>
2024-06-26 19:36:25 +0530

    reafractored JSON output

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ import requests
 
 # fetching coordiates from apiNinjas
 def getCoordinates(location):
-    ninja_api_key = "eaMfoS5VActkeXwmF7ZlVA==DCNlLx5Jzpc15KUX" #exposed intentionally
+    ninja_api_key = "eaMfoS5VActkeXwmF7ZlVA==DCNlLx5Jzpc15KUX"  # exposed intentionally
     headers = {"X-Api-Key": ninja_api_key}
     geocodes = requests.get(
         "https://api.api-ninjas.com/v1/geocoding", headers=headers, params=location
@@ -19,8 +19,8 @@ def getCoordinates(location):
 # providing response using sunsetsunrise.io API
 def getSunsetSunrise(request):
     if request.method == "GET":
-        city = request.GET.get('city', 'new delhi')
-        country = request.GET.get('country', "india")
+        city = request.GET.get("city", "new delhi")
+        country = request.GET.get("country", "india")
         print(city)
         location = {"city": city, "country": country}
         lattitude, longitude = getCoordinates(location)
@@ -32,12 +32,12 @@ def getSunsetSunrise(request):
 
         results = response.json()["results"]
         sunsetNsunrise = {
-            f"sunrise in {location['city']}": results['sunrise'],
-            f"sunset in {location['city']}": results['sunset']
-            
+            "location":{"city":city, "country":country},
+            "time": {"sunrise": results["sunrise"], "sunset": results["sunset"]}
         }
         return JsonResponse(sunsetNsunrise)
     else:
         return {request.method: "only GET requests allowed at this endpoint"}
 
-#will work on error handeling later.
+
+# will work on error handeling later.
